941090e188289d2520e19749ee078b50d00668af
MaciejSkrabski <maciej.skrabski@gmail.com>
2019-12-01 19:51:42 +0100

    Creating views and starting to write adapter

--- app/src/main/java/com/example/projekt/MainActivity.kt
+++ app/src/main/java/com/example/projekt/MainActivity.kt
@@ -2,11 +2,37 @@ package com.example.projekt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listaZadan.layoutManager = LinearLayoutManager(this)
+
+        //połączenie między bazą danych a widokiem
+        listaZadan.adapter
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId==R.id.dodaj){
+            Toast.makeText(this, "Dodaj!", Toast.LENGTH_LONG).show();
+            //Przejdź do fragmentu formularza dodania zadania
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/example/projekt/zadanie.kt
@@ -0,0 +1,12 @@
+package com.example.projekt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class zadanie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_zadanie)
+    }
+}
