5fa7ee48813f113579825fff517611382bd0a7ba
Maciej Skrabski <maciej.skrabski@gmail.com>
2019-12-02 11:06:56 +0100

    Form layout fix

--- app/src/main/java/com/example/projekt/MainActivity.kt
+++ app/src/main/java/com/example/projekt/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,31 +16,43 @@ import kotlinx.android.synthetic.main.recycler_view_fragment.*
 class MainActivity : AppCompatActivity() {
 
 
+    val fm = supportFragmentManager
+    val ft = fm.beginTransaction()
+
+    fun openFragment(fragment: Fragment)   {
+        val fm = supportFragmentManager
+        val ft = fm.beginTransaction()
+        ft.replace(R.id.fragment_container, fragment);
+        ft.addToBackStack(null);
+        ft.commit();
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        openFragment(RecyclerViewFragment())
 
-
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-
-        val fragment = RecyclerViewFragment()
-        fragmentTransaction.add(R.id.fragment_container, fragment)
-
-
+        //fragmentTransaction.add(R.id.vertical_layout, fragment)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu, menu);
+        menuInflater.inflate(R.menu.menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId==R.id.dodaj){
-            Toast.makeText(this, "Dodaj!", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Dodaj!", Toast.LENGTH_LONG).show()
+
             //Przejdź do fragmentu formularza dodania zadania
+
+            openFragment(FormLayoutFragment())
+
         }
+
+
         return super.onOptionsItemSelected(item)
     }
 }
